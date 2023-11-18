83b6801a8fd019b58db7eedc3180392dc5ee6a23
Didikkurniawan3 <didik2584@gmail.com>
2023-11-18 19:18:31 +0300

    first commit

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,19 @@
+class CommentRepository {
+  async addCommentToThread(data) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async verifyCommentAccess(commentId, owner) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async deleteComment(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+
+  async getCommentByThreadId(threadId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
