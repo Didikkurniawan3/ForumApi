796aa7b7caf0dd87a6e64c1c0b2390516634ac41
Didikkurniawan3 <didik2584@gmail.com>
2023-11-19 09:40:35 +0300

    test pass1 /GET

--- src/Infrastructures/http/_test/createServer.test.js
+++ src/Infrastructures/http/_test/createServer.test.js
@@ -22,7 +22,7 @@ describe('HTTP server', () => {
       const responseJson = JSON.parse(response.payload);
 
       expect(response.statusCode).toEqual(200);
-      expect(responseJson.value).toEqual('Hello world!');
+      expect(responseJson.value).toEqual('Hello Didik!');
     });
   });
 
